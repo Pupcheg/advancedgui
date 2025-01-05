dcf76ebd8e0322c74311739f76ab33f7ace4315c
Pupcheg <santos43241@gmail.com>
2025-01-06 00:40:15 +0300

    Removed copying from CollectionUtil#makeNoNullsList

--- advancedgui-api/src/main/java/me/supcheg/advancedgui/api/util/CollectionUtil.java
+++ advancedgui-api/src/main/java/me/supcheg/advancedgui/api/util/CollectionUtil.java
@@ -1,30 +1,57 @@
 package me.supcheg.advancedgui.api.util;
 
+import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 import org.jetbrains.annotations.Contract;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Unmodifiable;
 
-import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.AbstractList;
 import java.util.List;
 import java.util.Objects;
 
-@NoArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
 public final class CollectionUtil {
     @SafeVarargs
     @Unmodifiable
     @NotNull
     @Contract("_, _, _ -> new")
-    public static <T> List<T> makeNoNullsList(@NotNull T t1, @NotNull T t2, @NotNull T @NotNull ... other) {
-        if (other.length == 0) {
-            return List.of(t1, t2);
-        }
-        List<T> list = new ArrayList<>(2 + other.length);
-        list.add(Objects.requireNonNull(t1));
-        list.add(Objects.requireNonNull(t2));
-        list.addAll(Arrays.asList(other));
-
-        return List.copyOf(list);
+    public static <T> List<T> makeNoNullsList(@NotNull T first, @NotNull T second, @NotNull T @NotNull ... other) {
+        return new NoNullsFirstSecondOtherList<>(first, second, other);
     }
+
+    private static final class NoNullsFirstSecondOtherList<T> extends AbstractList<T> {
+        private final T first;
+        private final T second;
+        private final T[] other;
+
+        public NoNullsFirstSecondOtherList(
+                @NotNull T first,
+                @NotNull T second,
+                @NotNull T @NotNull [] other
+        ) {
+            this.first = Objects.requireNonNull(first);
+            this.second = Objects.requireNonNull(second);
+
+            for (T o : other) {
+                Objects.requireNonNull(o);
+            }
+            this.other = other;
+        }
+
+        @Override
+        public T get(int index) {
+            return switch (index) {
+                case 0 -> first;
+                case 1 -> second;
+                default -> other[index - 2];
+            };
+        }
+
+        @Override
+        public int size() {
+            return 2 + other.length;
+        }
+    }
+
 }
