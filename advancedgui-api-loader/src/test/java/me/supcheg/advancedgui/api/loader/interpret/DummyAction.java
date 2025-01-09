56b21b337ceda76e2f673c2a979690900773d183
Pupcheg <santos43241@gmail.com>
2025-01-09 10:00:02 +0300

    Made DummyAction scan classpath

--- advancedgui-api-loader/src/test/java/me/supcheg/advancedgui/api/loader/interpret/DummyAction.java
+++ advancedgui-api-loader/src/test/java/me/supcheg/advancedgui/api/loader/interpret/DummyAction.java
@@ -4,24 +4,18 @@ import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 import lombok.SneakyThrows;
 import me.supcheg.advancedgui.api.action.Action;
+import me.supcheg.advancedgui.api.loader.configurate.interpret.YamlClasspathActionInterpreterSource;
 import me.supcheg.advancedgui.api.loader.configurate.serializer.action.ActionTypeSerializer;
 import org.jetbrains.annotations.NotNull;
-import org.spongepowered.configurate.AttributedConfigurationNode;
 
-import java.util.stream.Stream;
+import static org.spongepowered.configurate.BasicConfigurationNode.root;
 
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public final class DummyAction {
     private static final String NAME = "dummy";
 
     private static final ActionTypeSerializer ACTION_TYPE_SERIALIZER = new ActionTypeSerializer(
-            () -> Stream.of(
-                    new ActionInterpreterEntry<>(
-                            NAME,
-                            new DummyActionInterpreter(),
-                            new DummyActionInterpretContextParser(NAME)
-                    )
-            )
+            new YamlClasspathActionInterpreterSource(DummyAction.class.getClassLoader())
     );
 
     @SafeVarargs
@@ -35,7 +29,7 @@ public final class DummyAction {
     @SneakyThrows
     public static <A extends Action> A dummyAction(@NotNull Class<A> type) {
         @SuppressWarnings("unchecked")
-        A action = (A) ACTION_TYPE_SERIALIZER.deserialize(type, AttributedConfigurationNode.root().set(NAME));
+        A action = (A) ACTION_TYPE_SERIALIZER.deserialize(type, root().set(NAME));
         return action;
     }
 }
