22550f5eb7ff7e39d2eba5b193b7992872d22622
Pupcheg <santos43241@gmail.com>
2025-02-01 17:00:56 +0300

    Added javadoc for ChestRowsSource

--- advancedgui-api/src/test/java/me/supcheg/advancedgui/api/annotation/ChestRowsSource.java
+++ advancedgui-api/src/test/java/me/supcheg/advancedgui/api/annotation/ChestRowsSource.java
@@ -1,5 +1,6 @@
 package me.supcheg.advancedgui.api.annotation;
 
+import me.supcheg.advancedgui.api.TestInventoryConstants;
 import org.junit.jupiter.params.provider.ArgumentsSource;
 
 import java.lang.annotation.ElementType;
@@ -7,6 +8,13 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * Provides single argument:
+ * <p>{@code int} from {@code 1} to {@link TestInventoryConstants#MAX_CHEST_INVENTORY_ROWS}
+ *
+ * @see ChestRowsArgumentsProvider
+ * @see ChestRowsSourceTests
+ */
 @Target({ElementType.ANNOTATION_TYPE, ElementType.METHOD})
 @Retention(RetentionPolicy.RUNTIME)
 @ArgumentsSource(ChestRowsArgumentsProvider.class)
