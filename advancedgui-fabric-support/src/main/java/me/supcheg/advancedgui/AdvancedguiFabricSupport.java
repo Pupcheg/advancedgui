f3528c3d6e6977e64d33914727a8f78ce69cfb45
Pupcheg <santos43241@gmail.com>
2025-05-24 14:02:33 +0300

    Added fabric mod

---
+++ advancedgui-fabric-support/src/main/java/me/supcheg/advancedgui/AdvancedguiFabricSupport.java
@@ -0,0 +1,9 @@
+package me.supcheg.advancedgui;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class AdvancedguiFabricSupport implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+    }
+}
